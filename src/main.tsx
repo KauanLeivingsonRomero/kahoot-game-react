f0caadc5b168097f2d26c27554ff37272aa3ae92
z3r078 <kauanromero70@protonmail.com>
2024-06-15 18:45:03 -0300

    feat: Add route

--- src/main.tsx
+++ src/main.tsx
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
 import { UserAgentProvider } from '@quentin-sommer/react-useragent'
-import { BrowserRouter } from 'react-router-dom'
+import { BrowserRouter, Route, Routes   } from 'react-router-dom'
 import PusherProvider from './contexts/pusherContext.tsx'
 import GameProvider from './contexts/gameContext.tsx'
 import PointsProvider from './contexts/pointsContext.tsx'
@@ -15,11 +15,13 @@ ReactDOM.createRoot(document.getElementById('root')!).render(
         <UserAgentProvider ua={window.navigator.userAgent}>
           <GameProvider>
             <PointsProvider>
-              <App />
+              <Routes>
+                <Route path="/" Component={App} />
+              </Routes>
             </PointsProvider>
           </GameProvider>
         </UserAgentProvider>
       </PusherProvider>
-    </React.StrictMode>,
+    </React.StrictMode>
   </BrowserRouter>
 )
