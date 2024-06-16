5ce3677923b2f1cc81588f022fef900ae8f622b7
z3r078 <kauanromero70@protonmail.com>
2024-06-16 01:03:34 -0300

    feat: Testing removing routes

--- src/main.tsx
+++ src/main.tsx
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
 import { UserAgentProvider } from '@quentin-sommer/react-useragent'
-import { BrowserRouter, Route, Routes   } from 'react-router-dom'
+import { BrowserRouter } from 'react-router-dom'
 import PusherProvider from './contexts/pusherContext.tsx'
 import GameProvider from './contexts/gameContext.tsx'
 import PointsProvider from './contexts/pointsContext.tsx'
@@ -15,9 +15,7 @@ ReactDOM.createRoot(document.getElementById('root')!).render(
         <UserAgentProvider ua={window.navigator.userAgent}>
           <GameProvider>
             <PointsProvider>
-              <Routes>
-                <Route path="/" Component={App} />
-              </Routes>
+              <App />
             </PointsProvider>
           </GameProvider>
         </UserAgentProvider>
