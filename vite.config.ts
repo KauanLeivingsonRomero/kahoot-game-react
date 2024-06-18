a1ea955a518be34e8b7b852659c1a7a74884600f
z3r078 <kauanromero70@protonmail.com>
2024-06-18 13:11:20 -0300

    feat: change base vite

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/kahoot-control-e-react/",
+  base: "/kahoot-control-e-react/dist",
   plugins: [react()],
 })
